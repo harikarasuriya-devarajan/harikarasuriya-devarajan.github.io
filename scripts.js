fdf82c8a615aa091518923da477d7175fae3c93f
Harikarasuriya Devarajan <hsesuriya@gmail.com>
2024-06-21 23:50:32 -0400

    Made changes in scripts.js

--- scripts.js
+++ scripts.js
@@ -5,14 +5,4 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(data => {
             document.getElementById("navbar-container").innerHTML = data;
         });
-
-    // Add smooth scrolling to all links
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function(e) {
-            e.preventDefault();
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth'
-            });
-        });
-    });
 });
