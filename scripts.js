993460fa8733e9528cb8d1e4f6cad654b97cffbc
Harikarasuriya Devarajan <hsesuriya@gmail.com>
2024-06-22 00:01:42 -0400

    Changes in css

--- scripts.js
+++ scripts.js
@@ -4,5 +4,15 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(response => response.text())
         .then(data => {
             document.getElementById("navbar-container").innerHTML = data;
+
+            // Highlight the active menu item
+            const currentLocation = location.href;
+            const menuItem = document.querySelectorAll('nav ul li a');
+            const menuLength = menuItem.length;
+            for (let i = 0; i < menuLength; i++) {
+                if (menuItem[i].href === currentLocation) {
+                    menuItem[i].className = "active";
+                }
+            }
         });
 });
